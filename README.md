96cce47e97e7c43a58aec091b0faa85268db0886
JunyangShao <351775009@qq.com>
2021-12-06 15:39:31 -0500

    test

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 nail grammar serialization and deserialization benchmarking for CS592-ADS LAB.
 
 please have libxml installed.
+
 please 'mv /usr/include/libxml2/libxml /usr/include/' to solve incorrect paths.
+
 please run the stat script in its root directory, I haven't make its path robust.
--- stat/plot.py
+++ stat/plot.py
@@ -6,17 +6,26 @@ import numpy as np
 import re
 
 # collect data
-# os.system('cd ../parser/ && sh compile.sh')
+os.system('cd ../parser/ && sh compile.sh')
 
-# os.system('../parser/a.out ../data/editlogs_100_complex.xml > 100-complex')
-# os.system('../parser/a.out ../data/editlogs_10000_complex.xml > 10000-complex')
-# os.system('../parser/a.out ../data/editlogs_1000000_complex.xml > 1000000-complex')
-# os.system('../parser/a.out ../data/editlogs_100_plain.xml > 100-plain')
-# os.system('../parser/a.out ../data/editlogs_10000_plain.xml > 10000-plain')
-# os.system('../parser/a.out ../data/editlogs_1000000_plain.xml > 1000000-plain')
-# os.system('../parser/a.out ../data/editlogs_100_moderate.xml > 100-moderate')
-# os.system('../parser/a.out ../data/editlogs_10000_moderate.xml > 10000-moderate')
-# os.system('../parser/a.out ../data/editlogs_1000000_moderate.xml > 1000000-moderate')
+os.system('../parser/a.out ../data/editlogs_10_complex.xml > 10-complex')
+os.system('../parser/a.out ../data/editlogs_100_complex.xml > 100-complex')
+os.system('../parser/a.out ../data/editlogs_1000_complex.xml > 1000-complex')
+os.system('../parser/a.out ../data/editlogs_10000_complex.xml > 10000-complex')
+os.system('../parser/a.out ../data/editlogs_100000_complex.xml > 100000-complex')
+os.system('../parser/a.out ../data/editlogs_1000000_complex.xml > 1000000-complex')
+os.system('../parser/a.out ../data/editlogs_10_plain.xml > 10-plain')
+os.system('../parser/a.out ../data/editlogs_100_plain.xml > 100-plain')
+os.system('../parser/a.out ../data/editlogs_1000_plain.xml > 1000-plain')
+os.system('../parser/a.out ../data/editlogs_10000_plain.xml > 10000-plain')
+os.system('../parser/a.out ../data/editlogs_100000_plain.xml > 100000-plain')
+os.system('../parser/a.out ../data/editlogs_1000000_plain.xml > 1000000-plain')
+os.system('../parser/a.out ../data/editlogs_10_moderate.xml > 10-moderate')
+os.system('../parser/a.out ../data/editlogs_100_moderate.xml > 100-moderate')
+os.system('../parser/a.out ../data/editlogs_1000_moderate.xml > 1000-moderate')
+os.system('../parser/a.out ../data/editlogs_10000_moderate.xml > 10000-moderate')
+os.system('../parser/a.out ../data/editlogs_100000_moderate.xml > 100000-moderate')
+os.system('../parser/a.out ../data/editlogs_1000000_moderate.xml > 1000000-moderate')
 
 compress_rate = dict()
 serialize_time = dict()
@@ -53,10 +62,10 @@ ax.set_title('complex logs compression - compare')
 ax.set_xscale('log')
 ax.set_ylabel('compression (%)')
 ax.set_xlabel('count of records')
-x_ax=[100,10000,1000000]
-y_ax_complex=[compress_rate['complex'][100], compress_rate['complex'][10000], compress_rate['complex'][1000000]]
-y_ax_plain=[compress_rate['plain'][100], compress_rate['plain'][10000], compress_rate['plain'][1000000]]
-y_ax_moderate=[compress_rate['moderate'][100], compress_rate['moderate'][10000], compress_rate['moderate'][1000000]]
+x_ax=[10,100,1000,10000,100000,1000000]
+y_ax_complex=[compress_rate['complex'][10],compress_rate['complex'][100],compress_rate['complex'][1000], compress_rate['complex'][10000],compress_rate['complex'][100000], compress_rate['complex'][1000000]]
+y_ax_plain=[compress_rate['plain'][10], compress_rate['plain'][100],compress_rate['plain'][1000], compress_rate['plain'][10000],compress_rate['plain'][100000], compress_rate['plain'][1000000]]
+y_ax_moderate=[compress_rate['moderate'][10], compress_rate['moderate'][100],compress_rate['moderate'][1000], compress_rate['moderate'][10000],compress_rate['moderate'][100000], compress_rate['moderate'][1000000]]
 ax.plot(x_ax, y_ax_complex, label='complex')  # Plot some data on the axes.
 ax.plot(x_ax, y_ax_moderate, label='moderate')  # Plot some data on the axes.
 ax.plot(x_ax, y_ax_plain, label='plain')  # Plot some data on the axes.
@@ -68,11 +77,11 @@ ax.set_title('complex logs compression - average')
 ax.set_xscale('log')
 ax.set_ylabel('compression (%)')
 ax.set_xlabel('count of records')
-x_ax=[100,10000,1000000]
-y_ax_complex=[compress_rate['complex'][100], compress_rate['complex'][10000], compress_rate['complex'][1000000]]
-y_ax_plain=[compress_rate['plain'][100], compress_rate['plain'][10000], compress_rate['plain'][1000000]]
-y_ax_moderate=[compress_rate['moderate'][100], compress_rate['moderate'][10000], compress_rate['moderate'][1000000]]
-y_ax=[(y_ax_moderate[0]+y_ax_complex[0]+y_ax_plain[0])/3,(y_ax_moderate[1]+y_ax_complex[1]+y_ax_plain[1])/3,(y_ax_moderate[2]+y_ax_complex[2]+y_ax_plain[2])/3]
+x_ax=[10,100,1000,10000,100000,1000000]
+y_ax_complex=[compress_rate['complex'][10],compress_rate['complex'][100],compress_rate['complex'][1000], compress_rate['complex'][10000],compress_rate['complex'][100000], compress_rate['complex'][1000000]]
+y_ax_plain=[compress_rate['plain'][10], compress_rate['plain'][100],compress_rate['plain'][1000], compress_rate['plain'][10000],compress_rate['plain'][100000], compress_rate['plain'][1000000]]
+y_ax_moderate=[compress_rate['moderate'][10],compress_rate['moderate'][100],compress_rate['moderate'][1000], compress_rate['moderate'][10000],compress_rate['moderate'][100000], compress_rate['moderate'][1000000]]
+y_ax=[(y_ax_moderate[0]+y_ax_complex[0]+y_ax_plain[0])/3,(y_ax_moderate[1]+y_ax_complex[1]+y_ax_plain[1])/3,(y_ax_moderate[2]+y_ax_complex[2]+y_ax_plain[2])/3,(y_ax_moderate[3]+y_ax_complex[3]+y_ax_plain[3])/3, (y_ax_moderate[4]+y_ax_complex[4]+y_ax_plain[4])/3, (y_ax_moderate[5]+y_ax_complex[5]+y_ax_plain[5])/3]
 ax.plot(x_ax, y_ax, label='average')  # Plot some data on the axes.
 ax.legend()
 fig.savefig('compress=rate-average.png')
@@ -83,10 +92,10 @@ ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_ylabel('time (us)')
 ax.set_xlabel('count of records')
-x_ax=[100,10000,1000000]
-y_ax_complex=[serialize_time['complex'][100], serialize_time['complex'][10000], serialize_time['complex'][1000000]]
-y_ax_plain=[serialize_time['plain'][100], serialize_time['plain'][10000], serialize_time['plain'][1000000]]
-y_ax_moderate=[serialize_time['moderate'][100], serialize_time['moderate'][10000], serialize_time['moderate'][1000000]]
+x_ax=[10,100,1000,10000,100000,1000000]
+y_ax_complex=[serialize_time['complex'][10],serialize_time['complex'][100],serialize_time['complex'][1000], serialize_time['complex'][10000],serialize_time['complex'][100000], serialize_time['complex'][1000000]]
+y_ax_plain=[serialize_time['plain'][10],serialize_time['plain'][100],serialize_time['plain'][1000], serialize_time['plain'][10000], serialize_time['plain'][100000], serialize_time['plain'][1000000]]
+y_ax_moderate=[serialize_time['moderate'][10],serialize_time['moderate'][100],serialize_time['moderate'][1000], serialize_time['moderate'][10000],serialize_time['moderate'][100000], serialize_time['moderate'][1000000]]
 ax.plot(x_ax, y_ax_complex, label='complex')  # Plot some data on the axes.
 ax.plot(x_ax, y_ax_moderate, label='moderate')  # Plot some data on the axes.
 ax.plot(x_ax, y_ax_plain, label='plain')  # Plot some data on the axes.
@@ -99,11 +108,11 @@ ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_ylabel('time (us)')
 ax.set_xlabel('count of records')
-x_ax=[100,10000,1000000]
-y_ax_complex=[serialize_time['complex'][100], serialize_time['complex'][10000], serialize_time['complex'][1000000]]
-y_ax_plain=[serialize_time['plain'][100], serialize_time['plain'][10000], serialize_time['plain'][1000000]]
-y_ax_moderate=[serialize_time['moderate'][100], serialize_time['moderate'][10000], serialize_time['moderate'][1000000]]
-y_ax=[(y_ax_moderate[0]+y_ax_complex[0]+y_ax_plain[0])/3,(y_ax_moderate[1]+y_ax_complex[1]+y_ax_plain[1])/3,(y_ax_moderate[2]+y_ax_complex[2]+y_ax_plain[2])/3]
+x_ax=[10,100,1000,10000,100000,1000000]
+y_ax_complex=[serialize_time['complex'][10],serialize_time['complex'][100],serialize_time['complex'][1000], serialize_time['complex'][10000],serialize_time['complex'][100000], serialize_time['complex'][1000000]]
+y_ax_plain=[serialize_time['plain'][10],serialize_time['plain'][100],serialize_time['plain'][1000], serialize_time['plain'][10000],serialize_time['plain'][100000], serialize_time['plain'][1000000]]
+y_ax_moderate=[serialize_time['moderate'][10],serialize_time['moderate'][100],serialize_time['moderate'][1000], serialize_time['moderate'][10000], serialize_time['moderate'][100000], serialize_time['moderate'][1000000]]
+y_ax=[(y_ax_moderate[0]+y_ax_complex[0]+y_ax_plain[0])/3,(y_ax_moderate[1]+y_ax_complex[1]+y_ax_plain[1])/3,(y_ax_moderate[2]+y_ax_complex[2]+y_ax_plain[2])/3,(y_ax_moderate[3]+y_ax_complex[3]+y_ax_plain[3])/3, (y_ax_moderate[4]+y_ax_complex[4]+y_ax_plain[4])/3, (y_ax_moderate[5]+y_ax_complex[5]+y_ax_plain[5])/3]
 y_ax_avg_se = y_ax
 ax.plot(x_ax, y_ax, label='average')  # Plot some data on the axes.
 ax.legend()
@@ -116,10 +125,10 @@ ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_ylabel('time (us)')
 ax.set_xlabel('count of records')
-x_ax=[100,10000,1000000]
-y_ax_complex=[deserialize_time['complex'][100], deserialize_time['complex'][10000], deserialize_time['complex'][1000000]]
-y_ax_plain=[deserialize_time['plain'][100], deserialize_time['plain'][10000], deserialize_time['plain'][1000000]]
-y_ax_moderate=[deserialize_time['moderate'][100], deserialize_time['moderate'][10000], deserialize_time['moderate'][1000000]]
+x_ax=[10,100,1000,10000,100000,1000000]
+y_ax_complex=[deserialize_time['complex'][10],deserialize_time['complex'][100],deserialize_time['complex'][1000], deserialize_time['complex'][10000],deserialize_time['complex'][100000], deserialize_time['complex'][1000000]]
+y_ax_plain=[deserialize_time['plain'][10],deserialize_time['plain'][100],deserialize_time['plain'][1000], deserialize_time['plain'][10000],deserialize_time['plain'][100000], deserialize_time['plain'][1000000]]
+y_ax_moderate=[deserialize_time['moderate'][10],deserialize_time['moderate'][100],deserialize_time['moderate'][1000], deserialize_time['moderate'][10000],deserialize_time['moderate'][100000], deserialize_time['moderate'][1000000]]
 ax.plot(x_ax, y_ax_complex, label='complex')  # Plot some data on the axes.
 ax.plot(x_ax, y_ax_moderate, label='moderate')  # Plot some data on the axes.
 ax.plot(x_ax, y_ax_plain, label='plain')  # Plot some data on the axes.
@@ -132,11 +141,11 @@ ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_ylabel('time (us)')
 ax.set_xlabel('count of records')
-x_ax=[100,10000,1000000]
-y_ax_complex=[deserialize_time['complex'][100], deserialize_time['complex'][10000], deserialize_time['complex'][1000000]]
-y_ax_plain=[deserialize_time['plain'][100], deserialize_time['plain'][10000], deserialize_time['plain'][1000000]]
-y_ax_moderate=[deserialize_time['moderate'][100], deserialize_time['moderate'][10000], deserialize_time['moderate'][1000000]]
-y_ax=[(y_ax_moderate[0]+y_ax_complex[0]+y_ax_plain[0])/3,(y_ax_moderate[1]+y_ax_complex[1]+y_ax_plain[1])/3,(y_ax_moderate[2]+y_ax_complex[2]+y_ax_plain[2])/3]
+x_ax=[10,100,1000,10000,100000,1000000]
+y_ax_complex=[deserialize_time['complex'][10],deserialize_time['complex'][100],deserialize_time['complex'][1000], deserialize_time['complex'][10000],deserialize_time['complex'][100000], deserialize_time['complex'][1000000]]
+y_ax_plain=[deserialize_time['plain'][10],deserialize_time['plain'][100],deserialize_time['plain'][1000], deserialize_time['plain'][10000],deserialize_time['plain'][100000], deserialize_time['plain'][1000000]]
+y_ax_moderate=[deserialize_time['moderate'][10],deserialize_time['moderate'][100],deserialize_time['moderate'][1000], deserialize_time['moderate'][10000],deserialize_time['moderate'][100000], deserialize_time['moderate'][1000000]]
+y_ax=[(y_ax_moderate[0]+y_ax_complex[0]+y_ax_plain[0])/3,(y_ax_moderate[1]+y_ax_complex[1]+y_ax_plain[1])/3,(y_ax_moderate[2]+y_ax_complex[2]+y_ax_plain[2])/3,(y_ax_moderate[3]+y_ax_complex[3]+y_ax_plain[3])/3, (y_ax_moderate[4]+y_ax_complex[4]+y_ax_plain[4])/3, (y_ax_moderate[5]+y_ax_complex[5]+y_ax_plain[5])/3]
 y_ax_avg_dese = y_ax
 ax.plot(x_ax, y_ax, label='average')  # Plot some data on the axes.
 ax.legend()
@@ -148,7 +157,7 @@ ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_ylabel('time (us)')
 ax.set_xlabel('count of records')
-x_ax=[100,10000,1000000]
+x_ax=[10,100,1000,10000,100000,1000000]
 ax.plot(x_ax, y_ax_avg_dese, label='deserialize')  # Plot some data on the axes.
 ax.plot(x_ax, y_ax_avg_se, label='serialize')  # Plot some data on the axes.
 ax.legend()
